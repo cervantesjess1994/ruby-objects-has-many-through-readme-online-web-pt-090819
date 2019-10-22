069749ce42d50b4d7fa0b791b9bcdc6316cf2005
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-10-22 11:11:07 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,30 +1,17 @@
 class Customer
+  class Customer
   attr_accessor :name, :age
-
+ 
   @@all = []
-
-  def initialize(name,age)
-    @name = "name"
+ 
+  def initialize(name, age)
+    @name = name
     @age = age
     @@all << self
   end
-
+ 
   def self.all
     @@all
   end
-
-  def new_meal(waiter, total, tip)
-    customer = self.new(name, age)
-    new_waiter = waiter.new(name, yrs_experience)
-    customer.new_meal(new_waiter, total, tip)
-  end
-
-  def meals
-    Meal.all.map {|meals| meals.customer == self}
-  end
-
-   def waiters
-     meals.map {|meals| meals.waiters}
-   end
-
+  
 end
