4816c1b850265200806e6ad747e15f28e7e8fa22
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-10-22 11:12:05 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,5 +1,4 @@
 class Waiter
-
   attr_accessor :name, :yrs_experience
 
   @@all = []
@@ -13,21 +12,5 @@ class Waiter
   def self.all
     @@all
   end
-
-  def new_meal(customer, total, tip)
-    self.new(customer, total, tip)
-  end
-
-  def meals
-    Meal.all.map {|meals| meals.waiter == self}
-   end
-
-   def best_tipper
-    # best_tipper =[]
-    # meals.map{|meals| meals.tip}
-    # best_tipper.to_a_max
-
-  #  best_tipped_meal = meals.max do |meal_a, meal_b|
-  #    meal_a.tip <=> meal_b.tip
-   end
- end
+ 
+end
